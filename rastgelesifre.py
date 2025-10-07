9bc92b0f418510273e0bb58cf2455486e0ff408e
EgeKaraveziroglu <egekaravezir@gmail.com>
2025-10-07 19:28:36 +0300

    Update rastgelesifre.py

--- rastgelesifre.py
+++ rastgelesifre.py
@@ -1,10 +1,14 @@
 import random
 karakterler="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-cevap =input("kaç karakter uzunlukta olsun maksimum 10")
-if cevap.isdigit:
-    uzunluk = int(cevap)
-    if 1 <= uzunluk <= 10:
-        sifre = ""
-        for i in range(uzunluk):
-            sifre += random.choice(karakterler)
-        print(sifre)
+while True:
+    cevap =input("kaç karakter uzunlukta olsun maksimum 10 çıkmak istersen çıkış de")
+    if cevap == "çıkış":
+        print("görüşürüz")   
+        break
+    if cevap.isdigit():
+        uzunluk = int(cevap)
+        if 1 <= uzunluk <= 10:
+            sifre = ""
+            for i in range(uzunluk):
+                sifre += random.choice(karakterler)
+            print(sifre)

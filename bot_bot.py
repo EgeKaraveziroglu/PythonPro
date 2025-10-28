ffd22fc14faa59a7b494ad92668c0b8bfd166f45
EgeKaraveziroglu <egekaravezir@gmail.com>
2025-10-28 19:58:53 +0300

    Update bot_bot.py

--- bot_bot.py
+++ bot_bot.py
@@ -1,3 +1,5 @@
+import os
+import random
 import discord
 from discord.ext import commands
 
@@ -12,14 +14,34 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!') 
 
+@bot.command()
+async def yardım(ctx):
+    await ctx.send(f"merhaba demek için $help hehehhehhe yazdırmak için $heh")
 
-
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f":D görüşürüz")
 
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def ha(ctx, count_ha = 5):
+    await ctx.send("ha" * count_ha)
+
+
+
+@bot.command()
+async def mem(ctx):
+    images= os.listdir("mem")
+    img_name = random.choice(images)
+    with open(f'mem/{img_name}', 'rb') as f:
+            picture = discord.File(f) 
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
 
 bot.run("")
